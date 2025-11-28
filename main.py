1b98bfc7807616a8cacbeac605458c18f65bb104
Maddoss <maddoss539@gmail.com>
2025-11-29 04:19:47 +0900

    Trying new Git

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Hle  lo, World!")
+
+import torch
+import torch.nn as nn
+import torch.optim as optim 
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+
+print("this works for most of the Machine learnings pojrects ")
